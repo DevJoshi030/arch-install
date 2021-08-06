0db40ca8ee7ce25682aeb7ea623e8e8e9d266f27
Dev Joshi <devjoshi030@gmail.com>
2021-08-06 10:22:28 +0000

    Install KDE Script

---
+++ install_kde.sh
@@ -0,0 +1,20 @@
+sudo timedatectl set-ntp true
+sudo hwclock --systohc
+
+git clone https://aur.archlinux.org/pikaur.git
+cd pikaur/
+makepkg -si --noconfirm
+
+#pikaur -S --noconfirm system76-power
+#sudo systemctl enable --now system76-power
+#sudo system76-power graphics integrated
+#pikaur -S --noconfirm auto-cpufreq
+#sudo systemctl enable --now auto-cpufreq
+
+
+sudo pacman -S --noconfirm xorg sddm plasma kde-applications firefox vlc
+
+sudo systemctl enable sddm
+/bin/echo -e "\e[1;32mREBOOTING IN 5..4..3..2..1..\e[0m"
+sleep 5
+reboot

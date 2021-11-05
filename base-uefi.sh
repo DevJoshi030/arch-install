a1943b6119b51445d402a206c88e12c55588d4e6
Dev Joshi <devjoshi030@gmail.com>
2021-11-05 16:00:03 +0000

    Update base-mbr.sh, base-uefi.sh, cinnamon.sh, dwm.sh, gnome.sh, kde.sh, LICENSE, xfce.sh, bspwm/bspwm.sh, bspwm/bspwmrc, bspwm/sxhkdrc Deleted install_arch.sh, install_kde.sh

--- base-uefi.sh
+++ base-uefi.sh
@@ -2,35 +2,40 @@
 
 ln -sf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
 hwclock --systohc
+sed -i '177s/.//' /etc/locale.gen
+locale-gen
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 echo "arch" >> /etc/hostname
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
-echo root:password | chpasswd
+echo root:root | chpasswd
 
 # You can add xorg to the installation packages, I usually add it at the DE or WM install script
 # You can remove the tlp package if you are installing on a desktop or vm
-pacman -Sy
-pacman -S grub efibootmgr os-prober ntfs-3g networkmanager network-manager-applet wireless_tools wpa_supplicant dialog mtools dosfstools base-devel linux-headers git vim bluez bluez-utils pulseaudio-bluetooth openssh
+
+pacman -S grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools base-devel linux-headers bluez bluez-utils pipewire pipewire-alsa pipewire-jack bash-completion openssh virt-manager qemu qemu-arch-extra edk2-ovmf os-prober ntfs-3g terminus-font
 
 # pacman -S --noconfirm xf86-video-amdgpu
-# pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
+pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
 
-grub-install --target=x86_64-efi --efi-directory=/boot/ --bootloader-id=GRUB
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 
 systemctl enable NetworkManager
 systemctl enable bluetooth
 systemctl enable sshd
+systemctl enable libvirtd
 
-useradd -m devjoshi
-echo devjoshi:password | chpasswd
-# usermod -aG libvirt devjoshi
+useradd -m dev 
+echo dev:devjoshi | chpasswd
+usermod -aG libvirt dev
 
-echo "devjoshi ALL=(ALL) ALL" >> /etc/sudoers.d/devjoshi
+echo "dev ALL=(ALL) ALL" >> /etc/sudoers.d/dev
 
 
 printf "\e[1;32mDone! Type exit, umount -a and reboot.\e[0m"
+
+
 
 
--- kde.sh
+++ kde.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 sudo timedatectl set-ntp true
 sudo hwclock --systohc
 
@@ -12,9 +14,16 @@ makepkg -si --noconfirm
 #sudo systemctl enable --now auto-cpufreq
 
 
-sudo pacman -S --noconfirm xorg sddm plasma kde-applications firefox vlc
+sudo pacman -S --noconfirm xorg sddm plasma kde-applications firefox vlc papirus-icon-theme materia-kde
+
+pikaur -S ttf-ms-fonts
 
 sudo systemctl enable sddm
+
+balooctl disable
+balooctl purge
+balooctl status
+
 /bin/echo -e "\e[1;32mREBOOTING IN 5..4..3..2..1..\e[0m"
 sleep 5
 reboot
